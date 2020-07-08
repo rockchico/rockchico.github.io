6e5d6e75408e426c25884f12427634222e213024
Francisco Schwertner <franciscos@universo.univates.br>
2020-07-08 10:36:45 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8076a69abad86e11049c.js"
+    "url": "webpack-runtime-a9cdf2ab2596d2529232.js"
   },
   {
     "url": "framework-95b680cb3190aa9bfe59.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "debe41670527411f17912602384582e9"
+    "revision": "1993c4ecc8e7b5285f4026c74635de68"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
