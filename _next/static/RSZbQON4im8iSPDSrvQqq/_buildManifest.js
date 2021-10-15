259a859a8ab636567c954c93377446fe18bf8bdf
Francisco <rockchico@gmail.com>
2021-10-15 15:15:30 -0300

    index ok

---
+++ _next/static/RSZbQON4im8iSPDSrvQqq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/652-7b41237c29de698c1fd4.js","static/chunks/pages/index-76f4ad37827553cc1e8e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
