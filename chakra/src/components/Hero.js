7b586455563f4453dc655045af6d5d6f3266821b
Francisco <rockchico@gmail.com>
2021-10-15 14:54:40 -0300

    COM RESUMME

---
+++ chakra/src/components/Hero.js
@@ -0,0 +1,70 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+
+export const Hero = ({ title }) => (
+
+  <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+    <Flex p={8} flex={1} align={'center'} justify={'center'}>
+      <Stack spacing={6} w={'full'} maxW={'lg'}>
+        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+          <Text color={'blue.400'} as={'span'} size={'8vh'}>
+            Francisco Schwertner
+          </Text>{' '}
+          <Text
+            as={'span'}
+            position={'relative'}
+            _after={{
+              content: "''",
+              width: 'full',
+              height: useBreakpointValue({ base: '20%', md: '30%' }),
+              position: 'absolute',
+              bottom: 1,
+              left: 0,
+              bg: 'blue.400',
+              zIndex: -1,
+            }}>
+            Freelance
+          </Text>
+          <br />{' '}
+
+        </Heading>
+        <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+          Desenvolvimento Web, React/Node
+        </Text>
+        <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+          <Button
+            rounded={'full'}
+            bg={'blue.400'}
+            color={'white'}
+            _hover={{
+              bg: 'blue.500',
+            }}>
+            CV / Resumee
+          </Button>
+
+        </Stack>
+      </Stack>
+    </Flex>
+    <Flex flex={1}>
+      <Image
+        alt={'Login Image'}
+        objectFit={'cover'}
+        src={
+          'https://images.unsplash.com/photo-1579361746237-3c8054b195e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+        }
+      />
+    </Flex>
+  </Stack>
+
+)
+
+Hero.defaultProps = {
+  title: 'Francisco Schwertner',
+}
